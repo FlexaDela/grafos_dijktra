3b76bfeb265e8abd6d9f3584045b9b6e6be42082
FlexaDela <mateusflexa2000@gmail.com>
2025-07-19 21:46:40 -0300

    o programa funciona

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ import folium
 
 
 # Baixa o grafo de ruas de Macapá (Amapá)
-place = "Macapá, Amapá, Brasil"
-graph = ox.graph_from_place(place, network_type="drive")  # Rede viária para carros
+place = "Macapá, Amapá, Brazil"
+graph = ox.graph_from_place(place, network_type="drive_service")  # Rede viária para carros
 
-# Converte para um grafo do NetworkX
-G = ox.utils_graph.get_undirected(graph)
 
 # Encontra os nós (pontos) mais próximos de dois locais
-origem = ox.distance.nearest_nodes(G, -51.0654, 0.0346)  # Coordenadas aproximadas do Centro
-destino = ox.distance.nearest_nodes(G, -51.0923, 0.0521)   # Coordenadas aproximadas de um bairro periférico
+origem = ox.distance.nearest_nodes(graph , -51.0654, 0.0346)  # Coordenadas aproximadas do Centro
+destino = ox.distance.nearest_nodes(graph , -51.0923, 0.0521)   # Coordenadas aproximadas de um bairro periférico
 
 # Calcula o caminho mais curto com Dijkstra
-caminho = nx.shortest_path(G, origem, destino, weight="length")  # "length" = distância em metros
+caminho = nx.shortest_path(graph, origem, destino, weight="length")  # "length" = distância em metros
 
 # Extrai coordenadas do caminho
-coordenadas = [(G.nodes[n]["y"], G.nodes[n]["x"]) for n in caminho]
+coordenadas = [(graph.nodes[n]["y"], graph.nodes[n]["x"]) for n in caminho]
 
 # Cria um mapa centrado em Macapá
 mapa = folium.Map(location=[0.0346, -51.0654], zoom_start=13)
